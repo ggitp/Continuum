34d4c546a269b5fee6acca042ddabf9a5cadc6b0
George <deiroweby@gmail.com>
2026-06-19 02:30:38 +0300

    initial character gameplay

--- Scripts/player_animator.gd
+++ Scripts/player_animator.gd
@@ -1,30 +1,105 @@
 extends Node2D
 
-@export var player_controller : PlayerController
-@export var animation_player : AnimationPlayer
-@export var sprite : Sprite2D
+@export var player : PlayerController
+@export var sprite : AnimatedSprite2D
 
-func _process(delta: float) -> void:
-	#flips the character sprite based on direction
-	if player_controller.direction == 1:
-		sprite.flip_h = false
-	elif player_controller.direction == -1:
-		sprite.flip_h = true
+
+var current_animation: StringName = &""
+
+
+func _process(_delta: float) -> void:
+	_update_facing()
 	
-	#plays running animation
-	if abs(player_controller.velocity.x) > 0.0:
-		animation_player.play("run")
-	else:
-		animation_player.play("idle")
+	var desired_animation := _get_desired_animation()
 	
-	#plays jumping animation
-	if player_controller.velocity.y < 0.0:
-		animation_player.play("jump")
-	elif player_controller.velocity.y > 0.0:
-		animation_player.play("falling")
+	if desired_animation != current_animation:
+		_play_animation(desired_animation)
 	
 	
 	
+
+
+func _update_facing() -> void:
+	sprite.flip_h = player.facing_direction < 0
+
+
+
+func _get_desired_animation() -> StringName:
+	# Action animations have priority over movement animations.
+	match player.action_state:
+		PlayerController.ActionState.ATTACK_1:
+			return &"attack_1"
+		
+		PlayerController.ActionState.ATTACK_3:
+			return &"attack_3"
+		
+		PlayerController.ActionState.AIRBORNE_ATTACK:
+			return &"air_attack"
+		
+		PlayerController.ActionState.AIRBORNE_ATTACK_UP:
+			return &"air_attack_up"
+		
+		PlayerController.ActionState.AIRBORNE_ATTACK_DOWN:
+			return &"air_attack_down"
+		
+		PlayerController.ActionState.DASHING_ATTACK:
+			return &"dash_attack"
+		
+		PlayerController.ActionState.GOT_HIT:
+			return &"hurt"
+		
+		PlayerController.ActionState.DEAD:
+			return &"dead"
+		
+	match player.movement_state:
+		PlayerController.MovementState.DASHING:
+			return &"dash"
+		
+		PlayerController.MovementState.AIRBORNE:
+			if player.velocity.y < 0:
+				return &"jump"
+			else:
+				return &"fall"
+		
+		PlayerController.MovementState.GROUNDED:
+			if abs(player.velocity.x) > 1.0:
+				return &"run"
+			else:
+				return &"idle"
+
+	return &"idle"
+
+
+func _play_animation(animation_name: StringName) -> void:
+	current_animation = animation_name
 	
-	
-	
+	sprite.offset = _get_animation_offset(animation_name)
+	sprite.play(animation_name)
+
+
+func _get_animation_offset(animation_name: StringName) -> Vector2:
+	match animation_name:
+		&"jump":
+			return Vector2(0, -6)
+		
+		&"air_attack_up":
+			return Vector2(0, -10)
+		
+		&"air_attack_down":
+			return Vector2(0, -4)
+		
+		_:
+			return Vector2.ZERO
+
+
+
+
+
+
+
+
+
+
+
+
+#END
--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,89 +1,578 @@
 extends CharacterBody2D
 class_name PlayerController
 
-@export var speed := 10.0
-@export var jump_power := 10.0
-@export var camera : Camera2D
-
-var speed_multiplier := 30.0
-var movement_while_attacking_multiplier := 0.0
-var movement_while_not_attacking_multiplier := 30.0
-var is_attacking := false
-var jump_multiplier := -30.0
-var direction := 0
-var speed_tween : Tween
-
-#character sheet slice 128 down, 240 side
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
+signal movement_state_changed(previous_state, new_state)
+signal action_state_changed(previous_state, new_state)
 
 
-#func _ready() -> void:
+enum MovementState {
+	GROUNDED,
+	AIRBORNE,
+	DASHING
+}
+
+
+enum ActionState {
+	DEFAULT,
 	
+	ATTACK_1,
+	ATTACK_3,
+	
+	AIRBORNE_ATTACK,
+	AIRBORNE_ATTACK_UP,
+	AIRBORNE_ATTACK_DOWN,
+	
+	DASHING_ATTACK,
+	
+	GOT_HIT,
+	DEAD
+}
+
+
+@export_category("Movement")
+@export var move_speed := 300.0
+@export var ground_acceleration := 2200.0
+@export var ground_deceleration := 3600.0
+@export var air_acceleration := 1200.0
+@export var jump_velocity := -600.0
+
+
+@export_category("Dash")
+@export var dash_speed := 750.0
+@export var dash_duration := 0.16
+
+
+@export_category("Combat")
+@export_range(0.0, 1.0) var attack_movement_multiplier := 0.15
+@export var hurt_deceleration := 900.0
+
+
+@export_category("Drop Through")
+@export_range(1, 32, 1) var one_way_platform_layer := 3
+@export var drop_through_duration := 0.15
+@export var drop_through_velocity := 80.0
+
+
+@export_category("References")
+@export var camera: Camera2D
+
+
+var movement_state: MovementState = MovementState.GROUNDED
+var action_state: ActionState = ActionState.DEFAULT
+
+var movement_state_time := 0.0
+var action_state_time := 0.0
+
+# Current horizontal input: -1.0, 0.0 or 1.0.
+var move_input := 0.0
+
+# Where the player is looking. This remains -1 or 1 while standing still.
+var facing_direction := 1
+
+var drop_through_time := 0.0
+
+
+
+func _ready() -> void:
+	_enter_movement_state(movement_state)
+	_enter_action_state(action_state)
 
 
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	_update_drop_through(delta)
+	_read_input()
+	
+	_update_action_state(delta)
+	_update_movement_state(delta)
+	
+	_apply_gravity(delta)
+	
+	move_and_slide()
+	
+	_resolve_movement_state()
+
+
+
+
+func _read_input() -> void:
+	move_input = Input.get_axis("left", "right")
+	
+	if move_input != 0:
+		facing_direction = int(sign(move_input))
+	
+	if action_state in [
+		ActionState.GOT_HIT,
+		ActionState.DEAD
+	]:
+		return
+	
+	if Input.is_action_just_pressed("jump"):
+		if Input.is_action_pressed("down") and is_on_floor():
+			_start_drop_through()
+		elif _can_jump():
+			_jump()
+	
+	if Input.is_action_just_pressed("dash"):
+		_request_dash()
+	
+	if Input.is_action_just_pressed("attack"):
+		_request_attack()
+
+
+#
+##				ACTION STATE
+#
+
+
+func _change_action_state(new_state: ActionState) -> void:
+	if action_state == new_state:
+		return
+	
+	if not _can_change_action_state(new_state):
+		return
+	
+	var old_state := action_state
+	
+	_exit_action_state(old_state)
+	
+	action_state = new_state
+	action_state_time = 0.0
+	
+	_enter_action_state(new_state)
+	action_state_changed.emit(old_state, new_state)
+
+
+func _enter_action_state(new_state: ActionState) -> void:
+	match new_state:
+		ActionState.DEFAULT:
+			pass
+		
+		ActionState.ATTACK_1:
+			action_state_time = 0.30
+		
+		ActionState.ATTACK_3:
+			action_state_time = 0.42
+		
+		ActionState.AIRBORNE_ATTACK:
+			action_state_time = 0.30
+		
+		ActionState.AIRBORNE_ATTACK_UP:
+			action_state_time = 0.35
+		
+		ActionState.AIRBORNE_ATTACK_DOWN:
+			action_state_time = 0.40
+		
+		ActionState.DASHING_ATTACK:
+			action_state_time = 0.28
+		
+		ActionState.GOT_HIT:
+			action_state_time = 0.25
+		
+		ActionState.DEAD:
+			velocity = Vector2.ZERO
+
+
+
+func _exit_action_state(old_state: ActionState) -> void:
+	match old_state:
+		ActionState.ATTACK_1, \
+		ActionState.ATTACK_3, \
+		ActionState.AIRBORNE_ATTACK, \
+		ActionState.AIRBORNE_ATTACK_UP, \
+		ActionState.AIRBORNE_ATTACK_DOWN, \
+		ActionState.DASHING_ATTACK:
+			# Disable attack hitboxes here later.
+			pass
+
+
+func _can_change_action_state(new_state: ActionState) -> bool:
+	if action_state == ActionState.DEAD:
+		return false
+	
+	if new_state == ActionState.DEAD:
+		return true
+	
+	if new_state == ActionState.GOT_HIT:
+		return true
+	
+	if action_state == ActionState.GOT_HIT:
+		return false
+	
+	if action_state == ActionState.DEFAULT:
+		return true
+	
+	# Ordinary attacks currently finish before another action starts.
+	return new_state == ActionState.DEFAULT
+
+
+func _get_action_movement_multiplier() -> float:
+	if _is_attack_state(action_state):
+		return attack_movement_multiplier
+	
+	return 1.0
+
+
+func _is_attack_state(state: ActionState) -> bool:
+	return state in [
+		ActionState.ATTACK_1,
+		ActionState.ATTACK_3,
+		ActionState.AIRBORNE_ATTACK,
+		ActionState.AIRBORNE_ATTACK_UP,
+		ActionState.AIRBORNE_ATTACK_DOWN,
+		ActionState.DASHING_ATTACK
+	]
+
+
+func _update_action_state(delta: float) -> void:
+	match action_state:
+		ActionState.DEFAULT:
+			pass
+		
+		ActionState.ATTACK_1, \
+		ActionState.ATTACK_3, \
+		ActionState.AIRBORNE_ATTACK, \
+		ActionState.AIRBORNE_ATTACK_UP, \
+		ActionState.AIRBORNE_ATTACK_DOWN, \
+		ActionState.DASHING_ATTACK:
+			_update_attack_action(delta)
+		
+		ActionState.GOT_HIT:
+			_update_got_hit(delta)
+		
+		ActionState.DEAD:
+			_update_dead()
+
+
+#
+##				ACTION STATE END
+#
+
+
+#
+##				MOVEMENT SPEED STATE
+#
+
+
+func _change_movement_state(new_state: MovementState) -> void:
+	if movement_state == new_state:
+		return
+	
+	var old_state := movement_state
+	
+	_exit_movement_state(old_state)
+	
+	movement_state = new_state
+	movement_state_time = 0.0
+	
+	_enter_movement_state(new_state)
+	movement_state_changed.emit(old_state, new_state)
+
+
+func _resolve_movement_state() -> void:
+	if movement_state == MovementState.DASHING:
+		return
+	
+	if is_on_floor():
+		_change_movement_state(MovementState.GROUNDED)
+	else:
+		_change_movement_state(MovementState.AIRBORNE)
+
+
+func _enter_movement_state(new_state: MovementState) -> void:
+	match new_state:
+		MovementState.GROUNDED:
+			pass
+		
+		MovementState.AIRBORNE:
+			pass
+		
+		MovementState.DASHING:
+			movement_state_time = dash_duration
+			velocity.y = 0
+
+
+func _exit_movement_state(old_state: MovementState) -> void:
+	match old_state:
+		MovementState.DASHING:
+			pass
+
+
+func _update_movement_state(delta: float) -> void:
+	# Hurt and death control their own velocity.
+	if action_state in [
+		ActionState.GOT_HIT,
+		ActionState.DEAD
+	]:
+		return
+
+	match movement_state:
+		MovementState.GROUNDED:
+			_update_horizontal_movement(
+				delta,
+				ground_acceleration,
+				ground_deceleration
+			)
+		
+		MovementState.AIRBORNE:
+			_update_horizontal_movement(
+				delta,
+				air_acceleration,
+				air_acceleration
+			)
+		
+		MovementState.DASHING:
+			_update_dash(delta)
+
+
+#
+##				MOVEMENT SPEED STATE END
+#
+
+#
+##				DROP THROUGH ONE WAY PLATFORM
+#
+
+
+#Start dropping through one way platform
+func _start_drop_through() -> void:
+	if drop_through_time > 0:
+		return
+	
+	drop_through_time = drop_through_duration
+	
+	set_collision_mask_value(one_way_platform_layer, false)
+	
+	velocity.y = max(velocity.y, drop_through_velocity)
+	_change_movement_state(MovementState.AIRBORNE)
+
+
+func _update_drop_through(delta: float) -> void:
+	if drop_through_time <= 0:
+		return
+	
+	drop_through_time -= delta
+	
+	if drop_through_time <= 0:
+		set_collision_mask_value(one_way_platform_layer, true)
+
+#
+##				DROP THROUGH ONE WAY PLATFORM END
+#
+
+#
+##				JUMP
+#
+
+
+func _can_jump() -> bool:
+	return (
+		is_on_floor()
+		and movement_state != MovementState.DASHING
+		and action_state == ActionState.DEFAULT
+	)
+
+func _jump() -> void:
+	velocity.y = jump_velocity
+	_change_movement_state(MovementState.AIRBORNE)
+
+
+#
+##				JUMP END
+#
+
+
+#
+##				DASH
+#
+
+
+
+func _request_dash() -> void:
+	if movement_state == MovementState.DASHING:
+		return
+	
+	if action_state != ActionState.DEFAULT:
+		return
+	
+	_change_movement_state(MovementState.DASHING)
+
+
+
+func _update_dash(delta: float) -> void:
+	movement_state_time -= delta
+	velocity.x = facing_direction * dash_speed
+	
+	if movement_state_time <= 0:
+		if is_on_floor():
+			_change_movement_state(MovementState.GROUNDED)
+		else:
+			_change_movement_state(MovementState.AIRBORNE)
+
+
+#
+##				DASH END
+#
+
+
+#
+##				GRAVITY APPLICATION
+#
+
+
+func _apply_gravity(delta: float) -> void:
+	if movement_state == MovementState.DASHING:
+		return
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	
-	# Handle down jump from 1 way platform
-	if Input.is_action_pressed("down") and Input.is_action_just_pressed("jump"):
-		_drop_through_platform()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = int(Input.get_axis("left", "right"))
-	if direction:
-		velocity.x = direction * speed * speed_multiplier
+
+#
+##				GRAVITY APPLICATION END
+#
+
+#
+##				ATTACK
+#
+
+func _request_attack() -> void:
+	if action_state != ActionState.DEFAULT:
+		return
+	
+	if movement_state == MovementState.DASHING:
+		_change_action_state(ActionState.DASHING_ATTACK)
+		return
+	
+	if movement_state == MovementState.AIRBORNE:
+		if Input.is_action_pressed("up"):
+			_change_action_state(ActionState.AIRBORNE_ATTACK_UP)
+		elif Input.is_action_pressed("down"):
+			_change_action_state(ActionState.AIRBORNE_ATTACK_DOWN)
+		else:
+			_change_action_state(ActionState.AIRBORNE_ATTACK)
+		
+		return
+	
+	_change_action_state(ActionState.ATTACK_1)
+
+
+
+func _update_attack_action(delta: float) -> void:
+	action_state_time -= delta
+	
+	if action_state_time <= 0:
+		_change_action_state(ActionState.DEFAULT)
+
+
+#
+##				ATTACK END
+#
+
+
+#
+##				HORIZONTAL MOVEMENT APPLICATION
+#
+
+
+
+func _update_horizontal_movement(
+	delta: float,
+	acceleration: float,
+	deceleration: float
+) -> void:
+	var movement_multiplier := _get_action_movement_multiplier()
+	var target_speed := move_input * move_speed * movement_multiplier
+
+	if move_input != 0:
+		velocity.x = move_toward(
+			velocity.x,
+			target_speed,
+			acceleration * delta
+		)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+		velocity.x = move_toward(
+			velocity.x,
+			0.0,
+			deceleration * delta
+		)
 
-	move_and_slide()
+
+#
+##				HORIZONTAL MOVEMENT APPLICATION END
+#
 
 
+#
+##				GOT HIT
+#
 
-func _input(event: InputEvent) -> void:
-	# Handle jump.
-	if event.is_action_pressed("jump") and is_on_floor() and is_attacking == false:
-		velocity.y = jump_power * jump_multiplier
+
+func _update_got_hit(delta: float) -> void:
+	action_state_time -= delta
 	
-	# Handle attack
-	if event.is_action_pressed("attack") and not is_attacking:
-		_start_attack()
+	velocity.x = move_toward(
+		velocity.x,
+		0.0,
+		hurt_deceleration * delta
+	)
 	
-	if event.is_action_released("attack"):
-		_stop_attack()
+	if action_state_time <= 0:
+		_change_action_state(ActionState.DEFAULT)
 
 
-func _start_attack():
-	is_attacking = true
-	
-	if speed_tween:
-		speed_tween.kill()
-	
-	speed_tween = create_tween()
-	speed_tween.tween_property(self, "speed_multiplier", movement_while_attacking_multiplier, 0.25)
+#
+##				GOT HIT END
+#
 
-func _stop_attack():
-	is_attacking = false
-	
-	if speed_tween:
-		speed_tween.kill()
-	
-	speed_tween = create_tween()
-	speed_tween.tween_property(self, "speed_multiplier", movement_while_not_attacking_multiplier, 0.15)
+
+func _update_dead() -> void:
+	velocity.x = 0
 
 
 
-func _drop_through_platform():
-	pass
 
-func _teleport_to_location(new_location):
+#
+##				TELEPORTATION TO LOCATION
+#
+
+
+func _teleport_to_location(new_location: Vector2) -> void:
 	camera.position_smoothing_enabled = false
-	position = new_location
+	global_position = new_location
+	
 	await get_tree().physics_frame
+	
 	camera.position_smoothing_enabled = true
+
+
+#
+##				TELEPORTATION TO LOCATION END
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#END
